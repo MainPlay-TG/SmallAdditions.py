edc9ecb69f61ed0a3b9dabb5f95b2fc58d696adc
MainPlay-YT <xbox.roman6666666666@gmail.com>
2024-05-19 18:51:24 +0700

    no-comment

--- TelebotUtils/lang.py
+++ TelebotUtils/lang.py
@@ -4,14 +4,26 @@ from typing import Union
 from telebot.types import *
 class lang:
   def __init__(self,path:str,dl:str="en"):
+    """Удобное получение текстов сообщений"""
     self.data={}
     self.dl=dl
     self.path=path
+    self.load=self.read
+    self.save=self.write
   def __getitem__(self,k):
     return self.data[k]
   def __setitem__(self,k,v):
     self.data[k]=v
   def __call__(self,*ps,**kw)->str:
+    """Получить текст для сообщения 
+    lang(name,**kw) -> текст сообщения из языка по умолчанию
+    lang(code,name,**kw) -> текст сообщения из указанного языка
+    lang(code,part,name,**kw) -> текст указанной категории из указанного языка
+    **kw - замена #$строк$#"""
+    if len(ps)==1:
+      code=self.dl
+      part="msg"
+      name=ps[0]
     if len(ps)==2:
       part="msg"
       name=ps[1]
@@ -19,13 +31,14 @@ class lang:
       part=ps[1]
       name=ps[2]
     else:
-      raise Exception("необходимо 2 или 3 аргумента")
-    if type(ps[0])==str:
-      code=ps[0]
-    if type(ps[0])==User:
-      code=ps[0].language_code
-    if type(ps[0])==Message:
-      code=ps[0].from_user.language_code
+      raise Exception("необходимо 1, 2 или 3 аргумента")
+    if len(ps)>1:
+      if type(ps[0])==str:
+        code=ps[0]
+      if type(ps[0])==User:
+        code=ps[0].language_code
+      if type(ps[0])==Message:
+        code=ps[0].from_user.language_code
     if not code in self.data:
       self.data[code]={}
     if not part in self.data[code]:
